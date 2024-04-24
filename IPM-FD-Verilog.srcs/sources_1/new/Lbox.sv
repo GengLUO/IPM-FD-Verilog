a2c419b0b540fcc548d78b2ec6ecd8425993efc1
Geng Luo <geng.luo@student.kuleuven.be>
2024-04-24 15:17:36 +0200

    store inverse of L in L box

--- IPM-FD-Verilog.srcs/sources_1/new/Lbox.sv
+++ IPM-FD-Verilog.srcs/sources_1/new/Lbox.sv
@@ -24,7 +24,8 @@ module Lbox #(
     parameter k = 1
 ) (
     input logic [$clog2(k):0] position,
-    output logic [7:0] L_prime[0:N-1]
+    output logic [7:0] L_prime[0:N-1],
+    output logic [7:0] L_prime_inv[0:N-1]
 );
 
   localparam N = n - k + 1;
@@ -35,6 +36,7 @@ module Lbox #(
   // logic [7:0] L[0:k-1][0:N-1];
   logic [7:0] L[0:k][0:N];
   // logic [7:0] L_inv[0:k-1][0:N-1];
+  logic [7:0] L_inv[0:k][0:N];
 
   always_comb begin
     case ({
@@ -44,12 +46,19 @@ module Lbox #(
         2'd1, 3'd1
       } : begin
         L[0][0] = 8'd1;
+
+
+        L_inv[0][0] = 8'd1;
       end
       {
         2'd1, 3'd2
       } : begin
         L[0][0] = 8'd1;
         L[0][1] = 8'd27;
+
+
+        L_inv[0][0] = 8'd1;
+        L_inv[0][1] = 8'hcc;
       end
       {
         2'd1, 3'd3
@@ -57,6 +66,11 @@ module Lbox #(
         L[0][0] = 8'd1;
         L[0][1] = 8'd27;
         L[0][2] = 8'd250;
+
+
+        L_inv[0][0] = 8'd1;
+        L_inv[0][1] = 8'hcc;
+        L_inv[0][2] = 8'h7d;
       end
       {
         2'd1, 3'd4
@@ -65,12 +79,22 @@ module Lbox #(
         L[0][1] = 8'd27;
         L[0][2] = 8'd250;
         L[0][3] = 8'd188;
+
+
+        L_inv[0][0] = 8'd1;
+        L_inv[0][1] = 8'hcc;
+        L_inv[0][2] = 8'h7d;
+        L_inv[0][3] = 8'hbd;
       end
       {
         2'd2, 3'd1
       } : begin
         L[0][0] = 8'd1;
         L[1][0] = 8'd1;
+
+
+        L_inv[0][0] = 8'd1;
+        L_inv[1][0] = 8'd1;
       end
       {
         2'd2, 3'd2
@@ -80,6 +104,13 @@ module Lbox #(
 
         L[1][0] = 8'd1;
         L[1][1] = 8'd188;
+
+
+        L_inv[0][0] = 8'd1;
+        L_inv[0][1] = 8'hcc;
+
+        L_inv[1][0] = 8'd1;
+        L_inv[1][1] = 8'hbd;
       end
       {
         2'd2, 3'd3
@@ -91,6 +122,15 @@ module Lbox #(
         L[1][0] = 8'd1;
         L[1][1] = 8'd239;
         L[1][2] = 8'd128;
+
+
+        L_inv[0][0] = 8'd1;
+        L_inv[0][1] = 8'hcc;
+        L_inv[0][2] = 8'h72;
+
+        L_inv[1][0] = 8'd1;
+        L_inv[1][1] = 8'hb3;
+        L_inv[1][2] = 8'h83;
       end
       ////////////////////////////////////DUMMY TEST FOR n=5, k=2/////////////////////////////////////////////////
       {
@@ -105,6 +145,17 @@ module Lbox #(
         L[1][1] = 8'd27;
         L[1][2] = 8'd250;
         L[1][3] = 8'd188;
+
+
+        L_inv[0][0] = 8'd1;
+        L_inv[0][1] = 8'h15;
+        L_inv[0][2] = 8'hd0;
+        L_inv[0][3] = 8'h0f;
+
+        L_inv[1][0] = 8'd1;
+        L_inv[1][1] = 8'hcc;
+        L_inv[1][2] = 8'h7d;
+        L_inv[1][3] = 8'hbd;
       end
       ////////////////////////////////////DUMMY TEST FOR n=5, k=2/////////////////////////////////////////////////
       default: begin
@@ -116,6 +167,7 @@ module Lbox #(
   always_comb begin
     for (int i = 0; i < N; i++) begin
       L_prime[i] = L[position][i];
+      L_prime_inv[i] = L_inv[position][i];
     end
   end
 
